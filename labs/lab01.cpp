89c4917813899d20f502d826aea363d6b9925aef
bf <bf@m-u1.mec.cuny.edu>
2020-02-11 14:31:17 -0500

    lab01

---
+++ labs/lab01.cpp
@@ -0,0 +1,28 @@
+/*
+  Date: Feb 11
+  Assignment: In-clas Lab01
+  Name: <Your First and Last Name>
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    // Todo:
+    // Task-0
+    
+    
+    // Task-1
+
+    
+    // Task-2
+
+    
+    // Task-3    
+    
+
+    return 0;
+
+} // end of the main function
