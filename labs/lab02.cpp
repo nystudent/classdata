8ae61010893e43596bc666cbf595ed004ff975da
bf <bf@m-u1.mec.cuny.edu>
2020-02-13 13:26:54 -0500

    lab02

---
+++ labs/lab02.cpp
@@ -0,0 +1,37 @@
+/*
+  Date: Feb 13
+  Assignment: In-class Lab01
+  Name: <Your First and Last Name>
+
+  To compile: g++ -o lab02 lab02.cpp
+  To execute: ./lab02
+
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+
+    // Task 0 Image
+    cout << "Image for task 0\n";
+    // todo
+
+
+
+
+    // Task 1 Image
+    cout << "\nImage for task 1\n";
+    // todo
+
+
+
+
+    // Task 2 Image
+    cout << "\nImage for taks 2\n";
+    // todo
+
+
+
+    return 0;
+}
