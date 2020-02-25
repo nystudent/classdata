65240f62aff841d3a5fa93e6a4328bcd81c5b57b
bf <bf@m-u1.mec.cuny.edu>
2020-02-25 16:57:26 -0500

    lab05.cpp

---
+++ labs/lab05.cpp
@@ -0,0 +1,121 @@
+/* 
+  assignment: lab05 
+  date: Feb25th
+  Student name: <your name>
+
+  to compile: g++ -o lab05 lab05.cpp
+  to execute: ./lab05 
+ 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+
+   cout << boolalpha;
+
+    // Task 0
+    // TODO:
+    // declare integer variable x
+    // declare two double variables s, t
+    // declare integer variable n
+    // declare integer variable dy
+    // declare string fariable val
+    // declare ineger variable yr
+
+    
+
+    //
+    // Task 1 Begins
+    cout << "Task 1\n";
+    cout << "Enter an integer: ";
+    cin >> x;
+
+    //Assign solution to task 1 to r1; 
+    bool r1 = false;
+    
+    cout << "It is " << r1 << " that " << x << " is a multiple of both 3 and 5\n\n";
+    // Task 1 Ends
+
+
+
+
+    //
+    // Task 2 Begins
+    cout << "Task 2\n";
+    cout << "Enter a number: ";
+    cin >> s;
+    cout << "Enter another number: ";
+    cin >> t;
+
+    //Assign solution to task 2 to r2; 
+    bool r2 = false;
+    
+    cout << "It is " << r2 << " that " << s << " and " << t << " have the same sign\n\n";
+    // Task 2 Ends
+
+
+
+
+    //
+    //  Task 3 Begins
+    cout << "Task 3\n";
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    //Assign solution to task 3 to r3; 
+    bool r3 = false;
+    
+    cout << "It is " << r3 << " that " << n << " is a three digit number\n\n";
+    // Task 3 Ends
+
+
+
+
+    //
+    //  Task 4 Begins 
+    cout << "Task 4\n";
+    cout << "Enter a day: ";
+    cin >> dy;
+
+    //Assign solution to task 4 to r4; 
+    bool r4 = false;
+    
+    cout << "It is " << r4 << " that " << dy << " fell on a Monday in the month of February 2020\n\n";
+    // Task 4 Ends
+
+    
+
+    //
+    // Task 5 Begins
+    cout << "Task 5\n";
+    cout << "Enter a word: ";
+    cin >> val;
+
+    //Assign solution to task 5 to r5; 
+    bool r5 = false;
+    
+    cout << "It is " << r5 << " that " << val << " equals some version of \"no\"\n\n";
+    // Task 5 Ends
+
+
+
+    //
+    // Task 6 Begins
+    cout << "Task 6\n";
+    cout << "Enter a year: ";
+    cin >> yr;
+
+    //Assign solution to task 6 to r6; 
+    bool r6 = false;
+    
+    cout << "It is " << r6 << " that " << yr << " is a leap year\n\n";
+    // Task 6 Ends
+
+
+
+    
+    return 0;
+}
