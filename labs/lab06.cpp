86035ccc0d222b0797016daf0b2f8a2346831ada
bf <bf@m-u1.mec.cuny.edu>
2020-02-27 10:57:00 -0500

    lab06

---
+++ labs/lab06.cpp
@@ -0,0 +1,44 @@
+/* 
+  Assignment: lab06
+  Date: Feb27th
+  Student name: <your name>
+
+  To compile: g++ -o lab06 lab06.cpp
+  To execute: ./lab06
+ 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{    
+    cout << "Task 0 \n";
+    // TODO:
+
+
+    // End of Task 0
+
+
+
+    cout << "\n Task 1\n";
+    // TODO:
+
+
+
+    // End of Task 1
+
+
+
+    cout << "\n Task 2\n";
+    // TODO:
+
+
+
+    // End of Task 2
+
+
+    return 0;
+}
+
