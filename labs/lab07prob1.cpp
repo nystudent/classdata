22f507f2a6e350c674e1cd1814544c85161510e3
bfilin <networkproject9@gmail.com>
2020-03-03 13:30:10 -0500

    lab07 problem1 and problem 2

---
+++ labs/lab07prob1.cpp
@@ -0,0 +1,29 @@
+/* 
+  Assignment: lab07 Problem-1
+  Date: March3rd
+  Student name: <your name>
+
+  To compile: g++ -o lab07prob1 lab07prob1.cpp
+  To execute: ./lab07prob1
+ 
+
+   TODO:
+   Write the Pseudocode for problem1  
+
+
+
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{    
+    // TODO:
+    // implement your Pseudocode in C++ 
+
+    return 0;
+}
+
---
+++ labs/lab07prob2.cpp
@@ -0,0 +1,29 @@
+/* 
+  Assignment: lab07 Problem-2
+  Date: March3rd
+  Student name: <your name>
+
+  To compile: g++ -o lab07prob2 lab07prob2.cpp
+  To execute: ./lab07prob2
+ 
+
+   TODO:
+   Write the Pseudocode for problem2
+
+
+
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{    
+    // TODO:
+    // implement your Pseudocode in C++ 
+
+    return 0;
+}
+
