c7a6ae1a227b8c509eb27ac6ea05661ecc418470
bfilin <networkproject9@gmail.com>
2020-03-10 16:35:26 -0400

    lab09

---
+++ labs/lab09.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    cout << "Task 0 \n";
+    // Start of Task 0 
+    // TODO
+	
+    // End of Task 0
+    
+
+
+
+    cout << "\nTask 1\n";
+    // Start of Task 1
+    // TODO
+
+    // End of Task 1
+    
+
+
+
+
+    cout << "\nTask 2\n";
+    // Start of Task 2
+    // TODO
+   
+
+    // End of Task 2
+    
+
+    return 0;
+}
