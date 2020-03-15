49d0c31056beeedeba41499bef72cd67b07163be
bfilin <networkproject9@gmail.com>
2020-03-15 13:40:43 -0400

    hw1

---
+++ Homework/hw1.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+
+    cout << "\nTask 1\n";
+    // Task 1 Begins
+    // TODO:
+
+
+    // Task 1 Ends
+
+
+    cout << "\nTask 2\n";
+    // Task 2 Begins
+    // TODO:
+
+    // Task 2 Ends
+
+
+    cout << "\nTask 3\n";
+    // Task 3 Begins
+    // TODO:
+
+    // Task 3 Ends
+
+
+    cout << "\nTask 4\n";
+    // Task 4 Begins
+    // TODO:
+
+    // Task 4 Ends
+
+ 
+    cout << "\nTask 5\n";
+    // Task 5 Begins
+    // TODO:
+
+    // Task 5 Ends
+
+
+    cout << "\nTask 6\n";
+    // Task 6 Begins
+    // TODO:
+
+    // Task 6 Ends
+
+
+    cout << "\nTask 7\n";
+    // Task 7 Begins
+    // TODO:
+
+    // Task 7 Ends
+
+
+    cout << "\nTask 8\n";
+    // Task 8 Begins
+    // TODO:
+
+    // Task 8 Ends
+
+
+    cout << "\nTask 9\n";
+    // Task 9 Begins
+    // TODO:
+
+    // Task 9 Ends
+
+
+
+    cout << "\nTask 10\n";
+    // Task 10 Begins
+    // TODO:
+
+    // Task 10Ends
+
+ 
+    cout << "\nTask 11\n";
+    // Task 11 Begins
+    // TODO:
+
+
+    // Task 11 Ends
+
+
+    cout << "\nTask 12\n";
+    // Task 12 Begins
+    // TODO:
+
+    // Task 12 Ends
+
+
+    cout << "\nTask 13\n";
+    // Task 13 Begins
+    // TODO:
+
+    // Task 13 Ends
+
+
+    cout << "\nTask 14\n";
+    // Task 14 Begins
+    // TODO:
+
+    // Task 14 Ends
+
+ 
+    cout << "\nTask 15\n";
+    // Task 15 Begins
+    // TODO:
+
+    // Task 15 Ends
+
+
+    cout << "\nTask 16\n";
+    // Task 16 Begins
+    // TODO:
+
+    // Task 16 Ends
+
+
+    cout << "\nTask 17\n";
+    // Task 17 Begins
+    // TODO:
+
+    // Task 17 Ends
+
+
+    cout << "\nTask 18\n";
+    // Task 18 Begins
+    // TODO:
+
+    // Task 18 Ends
+
+
+    cout << "\nTask 19\n";
+    // Task 19 Begins
+    // TODO:
+
+    // Task 19 Ends
+
+
+
+    cout << "\nTask 20\n";
+    // Task 20 Begins
+    // TODO:
+
+    // Task 20Ends
+
+  
+    cout << "\nTask 21\n";
+    // Task 21 Begins
+    // TODO:
+
+
+    // Task 21 Ends
+
+
+    cout << "\nTask 22\n";
+    // Task 22 Begins
+    // TODO:
+
+    // Task 22 Ends
+
+
+    cout << "\nTask 23\n";
+    // Task 23 Begins
+    // TODO:
+
+    // Task 23 Ends
+
+
+    cout << "\nTask 24\n";
+    // Task 24 Begins
+    // TODO:
+
+    // Task 24 Ends
+
+ 
+    cout << "\nTask 25\n";
+    // Task 25 Begins
+    // TODO:
+
+    // Task 25 Ends
+
+
+   
+    return 0;
+}
