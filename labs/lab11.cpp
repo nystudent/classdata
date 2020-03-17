592a02a94f2a1450c51c7fa4bbdc00a5f0d4a931
bfilin <networkproject9@gmail.com>
2020-03-17 17:39:41 -0400

    lab11

---
+++ labs/lab11.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    cout << "Task 0\n";
+    // Task 0 Begin
+    // TODO:
+
+    // Task 0 End
+
+    cout << "\nSection II\n";
+    // Task 1 Begin
+    // TODO:
+
+    // Task 1 End
+
+    return 0;
+}
